6f318037d12fd513eed200cb821599398b0e2aea
Nasir Abdulrasheed <mypythtesting@gmail.com>
2021-10-10 20:55:44 +0100

    handled +, space and # flags

--- test/main.c
+++ test/main.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int len1, len2, len3, len4;
 
-	len1 = _printf("%d + %i = %d\n", 5, 5, (55 + 5));
+	len1 = _printf("%+d + % i = %d\n", 5, 5, (55 + 5));
 	len2 = _printf("%d + %i = %d\n", -605, 434, (-605 + 434));
-	len3 = _printf("98(2): %b, 98(8): %o, %S\n", 98, 98, "Best\n\aSchool");
-	_printf("len1: %i, len2: %x, len3: %u\n", len1, &len2, -10);
-	printf("Len: %u 12: %x 250(8): %x or %d\n", -10, 12, 250, len3);
-	len4 = _printf("%p, %p, %r %k\n", (void *)0x6fff5100b608, NULL, "Nasir");
+	len3 = _printf("98(2): %#b, 98(8): %o, %S\n", 98, 98, "Best\n\aSchool");
+	_printf("len1: %i, len2: %p, len3: %u\n", len1, &len2, -10);
+	printf("Len: %u 12: %#x 250(8): %#x %+5d %k\n", -10, 12, 250, len3);
+	len4 = _printf("Pointer: %p, %p, %r %z\n", (void *)0x6fff5100b608, NULL, "Nasir");
 	_printf("%d\n", len4);
 
 	return (0);
